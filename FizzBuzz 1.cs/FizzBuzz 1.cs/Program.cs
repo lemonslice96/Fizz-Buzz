00afd5da8a18f380237a21fb73200ec97401f328
lemonslice96 <lemonslice96@gmail.com>
2014-03-07 09:57:02 -0500

    buzz

---
+++ FizzBuzz 1.cs/FizzBuzz 1.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz_1.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
